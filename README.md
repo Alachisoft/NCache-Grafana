cd4a260424e040a258c14989b23f054e57fbe3ce
Muhammad Haseeb <muhammad_haseeb@diyatech.com>
2022-04-13 16:14:12 +0500

    NCache Grafana App

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# NCache-Grafana
-Grafana dashboards for NCache
+### About
+
+This NCache Grafana app plugin provides a set of dashboards that allows a user to monitor NCache counters.
+NCache's app plugin uses Prometheus as datasource.
+
+### Dashboards
+
+ * Cache
+ * Client
+ * Bridge
+
+### Setup instructions
+
+
+To Setup Grafana for NCache monitoring you have to follow the following steps:
+
+	 * Add Prometheus as Datasource
+	 * Import Dashboards
+
+#### Add Prometheus as Datasource
+
+Prometheus is one of the officialy supported datasources for Grafana.
+Add Prometheus as datasource and name it 'Prometheus' because the default counters rely on datasource named Prometheus.
+
+#### Import dashboards
+
+After Enabling NCache's plugin goto Plugin Config and then goto Dashboards page.
+Import required dashboards from this page.
+
+#### Prometheus configuration
+
+Add all those nodes as targets for which you want to monitor NCache counters.
+For example, if you want to monitor `192.168.1.7` the config file will look like this:
+
+    scrape_configs:
+
+     - job_name: 'NCache'
+
+		# metrics_path defaults to '/metrics'
+		# scheme defaults to 'http'.
+
+		static_configs:
+		- targets: ['192.168.1.7:8255']
+
+
+##### v1.0.0
+- Preview version.
---
+++ module.js
@@ -0,0 +1,183 @@
+define(function() { return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./module.ts");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./legacy/config.ts":
+/*!**************************!*\
+  !*** ./legacy/config.ts ***!
+  \**************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.ExampleConfigCtrl = void 0;
+
+var ExampleConfigCtrl = /*#__PURE__*/function () {
+  ExampleConfigCtrl.$inject = ["$scope", "$injector"];
+
+  /** @ngInject */
+  function ExampleConfigCtrl($scope, $injector) {
+    _classCallCheck(this, ExampleConfigCtrl);
+
+    this.appEditCtrl.setPostUpdateHook(this.postUpdate.bind(this)); // Make sure it has a JSON Data spot
+
+    if (!this.appModel) {
+      this.appModel = {};
+    } // Required until we get the types sorted on appModel :(
+
+
+    var appModel = this.appModel;
+
+    if (!appModel.jsonData) {
+      appModel.jsonData = {};
+    }
+
+    console.log('ExampleConfigCtrl', this);
+  }
+
+  _createClass(ExampleConfigCtrl, [{
+    key: "postUpdate",
+    value: function postUpdate() {
+      var _a;
+
+      if (!((_a = this.appModel) === null || _a === void 0 ? void 0 : _a.enabled)) {
+        console.log('Not enabled...');
+        return;
+      } // TODO, can do stuff after update
+
+
+      console.log('Post Update:', this);
+    }
+  }]);
+
+  return ExampleConfigCtrl;
+}();
+
+exports.ExampleConfigCtrl = ExampleConfigCtrl;
+ExampleConfigCtrl.templateUrl = 'legacy/config.html';
+
+/***/ }),
+
+/***/ "./module.ts":
+/*!*******************!*\
+  !*** ./module.ts ***!
+  \*******************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.ConfigCtrl = void 0; // Angular pages
+
+var config_1 = __webpack_require__(/*! ./legacy/config */ "./legacy/config.ts");
+
+Object.defineProperty(exports, "ConfigCtrl", {
+  enumerable: true,
+  get: function get() {
+    return config_1.ExampleConfigCtrl;
+  }
+});
+
+/***/ })
+
+/******/ })});;
+//# sourceMappingURL=module.js.map
